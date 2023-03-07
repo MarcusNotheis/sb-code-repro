5f94ba62da2083cc89c30ba5122192a10fee1fe3
Marcus Notheis <marcus.notheis@sap.com>
2023-03-07 13:43:49 +0100

    initial commit

---
+++ .storybook/preview.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import type { Preview, Story } from '@storybook/react';
+
+const MyDecorator = (Story) => {
+  return (
+    <div>
+      <span>Hello Decorator!</span>
+      <Story />
+    </div>
+  );
+};
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'light',
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  // remove the next line, then `Show Code` displays the button as expected.
+  decorators: [MyDecorator],
+};
+
+export default preview;
